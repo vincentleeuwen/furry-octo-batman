52549d04bc4a27c181ca4f371a181bfa677076d5
Vincent van Leeuwen <vincentvanleeuwen@vincentleeuwen-4.local>
2015-05-08 16:08:51 +0200

    stackCardView added to CardView

---
+++ BitDateAmsterdam/CardsViewController.swift
@@ -0,0 +1,38 @@
+//
+//  CardsViewController.swift
+//  BitDateAmsterdam
+//
+//  Created by Vincent van Leeuwen on 08/05/15.
+//  Copyright (c) 2015 Vincent van Leeuwen. All rights reserved.
+//
+
+import UIKit
+
+class CardsViewController: UIViewController {
+
+    
+    @IBOutlet weak var cardStackView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ BitDateAmsterdam/SwipeView.swift
@@ -0,0 +1,43 @@
+//
+//  SwipeView.swift
+//  BitDateAmsterdam
+//
+//  Created by Vincent van Leeuwen on 08/05/15.
+//  Copyright (c) 2015 Vincent van Leeuwen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SwipeView: UIView {
+    private let card: CardView = CardView()
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initialize()
+    }
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    init() {
+        super.init(frame: CGRectZero)
+        initialize()
+    }
+    
+    private func initialize() {
+        self.backgroundColor = UIColor.clearColor()
+        self.addSubview(card)
+        
+        card.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Bottom, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Leading, multiplier: 1.0, constant: 0))
+        addConstraint(NSLayoutConstraint(item: card, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: self, attribute: NSLayoutAttribute.Trailing, multiplier: 1.0, constant: 0))
+    }
+}
